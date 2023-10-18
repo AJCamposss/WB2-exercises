45ac18437dcbb1f31f3a15cbb498dae6bf023460
AJCamposss <adrian.campos869@gmail.com>
2023-10-18 11:48:10 -0700

    completed area_of_circle exercise

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,7 @@
+let radius = 4;
+let area = Math.PI * Math.pow(radius, 2)
+// console.log(areaOfCircle)
+
+const areOfCircle = radius * area
+
+console.log(" The area of a circle with radius " + radius + " is " + area.toFixed(2))
