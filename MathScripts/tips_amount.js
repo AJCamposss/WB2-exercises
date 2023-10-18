3558a208139aad3e1e3f07f81ede935fe84779ad
AJCamposss <adrian.campos869@gmail.com>
2023-10-18 11:27:04 -0700

    Finished tip_amount exercise

---
+++ MathScripts/tips_amount.js
@@ -0,0 +1,9 @@
+let foodBill = 140;
+let tip = 0.20 * foodBill;
+// console.log(tip);
+
+const totalFoodBill = tip + foodBill;
+// console.log(totalFoodBill);
+
+console.log("The tip on a " + foodBill + " food bill is " + tip)
+
