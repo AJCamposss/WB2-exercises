84eed0bbeaa7b4a5eb60bbe079aeb5c2c4e35a45
AJCamposss <adrian.campos869@gmail.com>
2023-10-18 10:52:43 -0700

    finished area_of_rectangle exercise

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,6 @@
+let lengthOfRectangle = 9;
+let widthOfRectangle = 15;
+
+const areaOfRectangle = lengthOfRectangle * widthOfRectangle;
+
+console.log("The area of The Rectangle is " + areaOfRectangle)
