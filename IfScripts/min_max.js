357ae8ad82aa2fdfb222aebe7fa8c9145c73e51a
AJCamposss <adrian.campos869@gmail.com>
2023-10-19 14:31:20 -0700

    Completed exercise min_max

---
+++ IfScripts/min_max.js
@@ -0,0 +1,17 @@
+let a = 8;
+let b = 5;
+let c = 3;
+
+
+if (a>b && a>c) {
+    console.log("a is the largest number")
+}
+
+
+ if (c<a && c<b) {
+    console.log("c is the smallest number")
+}
+
+else {
+    console.log("The largest number" + a + "The smallest number" + c)
+}
---
+++ IfScripts/pay_rules.js
@@ -0,0 +1,13 @@
+let hoursWorked = 40;
+
+if (hoursWorked > 39) {
+    payRate = 12.50;
+} else if (hoursWorked == 40) {
+    payRate = 25.00;
+} else if (hoursWorked > 40) {
+    payRate = 37.40;
+}
+
+console.log(payRate);
+
+
